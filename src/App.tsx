80472130ee1fb744b2ab16421721d1f78a9c6aeb
Shaham Noorani <shahamnoorani@gmail.com>
2021-12-19 15:02:08 -0600

    resetting

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+import React, {useState, useEffect} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [response, setResponse] = useState("");
+
+  // Note: the empty deps array [] means
+  // this useEffect will run once
+  // similar to componentDidMount()
+  useEffect(() => {
+    fetch("http://localhost:5000/api/park")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setResponse(result.message);
+          console.log(result.message);
+        },
+        // Note: it's importa
+// instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [])
+
+  
+  if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <div>
+      {response}
+      </div>
+    );
+  }
+}
+
+export default App;
